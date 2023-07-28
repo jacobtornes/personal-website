f57b60ff48a66c816b9426ce884faa9b30ed5165
Jacob Tornes <jacob.emil.tornes@no.experis.com>
2023-07-28 14:43:17 +0200

    :sparkles: add vercelDeployTool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 
 const config = defineConfig({
     projectId: "o2bm68yy",
@@ -8,10 +9,8 @@ const config = defineConfig({
     title: "My personal website",
     apiVersion: "2023-06-27",
     basePath: "/admin",
-    plugins: [deskTool()],
+    plugins: [deskTool(), vercelDeployTool()],
     schema: { types: schemas }
-
-
 })
 
 export default config;
