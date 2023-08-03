f10b7d369c5563c135195c0f5efde2a9317406d3
Jacob Tornes <jacob.emil.tornes@no.experis.com>
2023-08-03 09:28:33 +0200

    :construction: add new hobbie page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,11 @@
-import { getProjects } from "@/sanity/sanity-utils";
+import { getHobbies, getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
 import Link from "next/link";
 
 
 export default async function Home() {
   const projects = await getProjects();
+  const hobbies = await getHobbies();
 
   return (
     <div>
@@ -21,7 +22,8 @@ export default async function Home() {
       </h1>
 
       <p className="mt-3 text-xl text-gray-600">
-        Aloha everyone! Check out my projects!
+        Aloha everyone! Below you can or can not check out 
+        my projects and hobbies.
       </p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
@@ -48,6 +50,35 @@ export default async function Home() {
           to-purple-600 bg-clip-text text-transparent"
             >
               {project.name}
+            </div>
+          </Link>
+        ))}
+      </div>
+
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Hobbies</h2>
+
+      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {hobbies.map((hobbie) => (
+          <Link
+            href={`/hobbies/${hobbie.slug}`}
+            key={hobbie._id}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 
+            hover:border-blue-500 transition"
+          >
+            {hobbie.image && (
+              <Image
+                src={hobbie.image}
+                alt={hobbie.name}
+                width={750}
+                height={300}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div
+              className="mt2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500
+          to-purple-600 bg-clip-text text-transparent"
+            >
+              {hobbie.name}
             </div>
           </Link>
         ))}
